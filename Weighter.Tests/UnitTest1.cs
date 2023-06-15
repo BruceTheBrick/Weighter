03df116473ad8f961e4a2be85c160d81cbdaeb8e
Bryce <techbryce@hotmail.com>
2023-06-15 16:42:13 +1000

    Initial Commit

---
+++ Weighter.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Weighter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Weighter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Weighter;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
