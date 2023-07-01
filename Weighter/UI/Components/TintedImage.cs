e46bfd67676a09d354ada1ec47e7f18bae938f0c
Bryce <techbryce@hotmail.com>
2023-07-02 09:19:08 +1000

    ADD - Implemented TintedImage.cs

---
+++ Weighter/UI/Components/TintedImage.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+
+namespace Weighter.UI.Components
+{
+    public class TintedImage : Image
+    {
+        public static readonly BindableProperty TintColorProperty = BindableProperty.Create(
+            nameof(TintColor),
+            typeof(Color),
+            typeof(TintedImage),
+            propertyChanged: TintColorChanged);
+
+        public TintedImage()
+        {
+        }
+
+        public Color TintColor
+        {
+            get => (Color)GetValue(TintColorProperty);
+            set => SetValue(TintColorProperty, value);
+        }
+
+        private static void TintColorChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            if (!(bindable is TintedImage image))
+            {
+                return;
+            }
+
+            ApplyTintBehavior(image, (Color)newvalue);
+        }
+
+        private static void ApplyTintBehavior(TintedImage image, Color tintColor)
+        {
+            var tintBehavior = image.Behaviors.FirstOrDefault(x => x.GetType() == typeof(IconTintColorBehavior));
+            if (tintBehavior != null)
+            {
+                image.Behaviors.Remove(tintBehavior);
+            }
+
+            image.Behaviors.Add(new IconTintColorBehavior { TintColor = tintColor });
+        }
+    }
+}
