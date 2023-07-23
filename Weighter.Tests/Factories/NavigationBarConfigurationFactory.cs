22d30c98d100efc870d9836ff30afdf0043ebcb2
Bryce <techbryce@hotmail.com>
2023-07-23 12:09:49 +1000

    ADD - Began to add NavigationBarConfigurationServiceTests.cs

---
+++ Weighter.Tests/Factories/NavigationBarConfigurationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using Weighter.Core.Models.UI;
+
+namespace Weighter.Tests.Factories
+{
+    public static class NavigationBarConfigurationFactory
+    {
+        public static NavigationBarConfiguration GetModel()
+        {
+            return GetModels(1).First();
+        }
+
+        public static IList<NavigationBarConfiguration> GetModels(int count = 5)
+        {
+            return new Faker<NavigationBarConfiguration>()
+                .RuleFor(x => x.Text, f => f.Random.Words())
+                .RuleFor(x => x.AccessibilityName, f => f.Random.Words())
+                .RuleFor(x => x.IconSource, f => f.Image.PlaceholderUrl(24, 24))
+                .RuleFor(x => x.IsInAccessibleTree, f => f.Random.Bool())
+                .Generate(count);
+        }
+    }
+}
---
+++ Weighter.Tests/Services/NavigationBarConfigurationServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Weighter.Core.Enums;
+using Weighter.Core.Services;
+using Weighter.Tests.Base;
+using Xunit;
+
+namespace Weighter.Tests.Services
+{
+    public class NavigationBarConfigurationServiceTests : UnitTestBase<NavigationBarConfigurationService>
+    {
+        #region Configuration
+
+        [Fact]
+        public void Configuration_ShouldReturnCloseConfiguration_WhenActionTypeIsClose()
+        {
+            //Arrange
+    
+            //Act
+            var configuration = NavigationBarConfigurationService.Configuration(NavigationBarActionType.Close);
+
+            //Assert
+        }
+
+        #endregion
+    }
+}
