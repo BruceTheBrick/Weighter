64de23965fad92c6e08c1110b68770f647957432
Bryce <techbryce@hotmail.com>
2023-07-23 11:00:06 +1000

    ADD - Added AppInitializationServiceTests.cs

---
+++ Weighter.Tests/Services/AppInitializationServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Weighter.Core.Databases.Interfaces;
+using Weighter.Core.Services;
+using Weighter.Tests.Base;
+using Xunit;
+
+namespace Weighter.Tests.Services
+{
+    public class AppInitializationServiceTests : UnitTestBase<AppInitializationService>
+    {
+        #region Initialize
+
+        [Fact]
+        public async Task Initialize_ShouldInitializeWeighterDatabase()
+        {
+            //Arrange
+
+            //Act
+            await Sut.Initialize();
+
+            //Assert
+            Mocker.GetMock<IWeighterDatabase>().Verify(x => x.Initialize());
+        }
+
+        [Fact]
+        public async Task Initialize_ShouldSetupThemeService_WhenCurrentIsNotNull()
+        {
+            //Arrange
+
+            //Act
+            await SutMock.Object.Initialize();
+
+            //Assert
+            SutMock.Verify(x => x.SetupThemeService());
+        }
+
+        #endregion
+    }
+}
--- Weighter/Core/Services/AppInitializationService.cs
+++ Weighter/Core/Services/AppInitializationService.cs
@@ -1,4 +1,5 @@
-﻿using Weighter.Core.Databases.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using Weighter.Core.Databases.Interfaces;
 using Weighter.Core.Services.Interfaces;
 
 namespace Weighter.Core.Services
@@ -6,14 +7,28 @@ namespace Weighter.Core.Services
     public class AppInitializationService : IAppInitializationService
     {
         private readonly IWeighterDatabase _weighterDatabase;
-        public AppInitializationService(IWeighterDatabase weighterDatabase)
+        private readonly IThemeService _themeService;
+        public AppInitializationService(
+            IWeighterDatabase weighterDatabase,
+            IThemeService themeService)
         {
             _weighterDatabase = weighterDatabase;
+            _themeService = themeService;
         }
 
         public async Task Initialize()
         {
             _weighterDatabase.Initialize();
+            SetupThemeService();
+        }
+
+        [ExcludeFromCodeCoverage]
+        public virtual void SetupThemeService()
+        {
+            if (Application.Current != null)
+            {
+                _themeService.Theme = Application.Current.RequestedTheme;
+            }
         }
     }
 }
